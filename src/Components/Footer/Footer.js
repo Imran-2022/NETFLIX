2159e9252b61255163a11cff6cd19bd3a082d2fa
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-05-15 11:36:17 +0600

    add footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,114 +3,122 @@ import "./Footer.css"
 import { FaFacebookSquare, FaGooglePlusSquare, FaLinkedin, FaTwitterSquare, IconName } from "react-icons/fa";
 const Footer = () => {
     return (
-        <div className="footer">
-            <div className="footer-in">
+     
 
-                <footer class="page-footer font-small mdb-color pt-4">
-
-                    <div class="container text-center text-md-left">
-
-                        <div class="row text-center text-md-left mt-3 pb-3">
-
-                            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                                <h6 class="text-uppercase mb-4 font-weight-bold">Company name</h6>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum ut culpa iure debitis totam, amet atque incidunt quidem ipsam iusto praesentium cum repellendus nisi? Quidem similique possimus earum dolore? Amet!</p>
-                            </div>
-
-                            <hr class="w-100 clearfix d-md-none" />
-
-                            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                                <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
-                                <p>
-                                    <a href="#!">Lorem</a>
-                                </p>
-                                <p>
-                                    <a href="#!">Lorem</a>
-                                </p>
-                                <p>
-                                    <a href="#!">Lorem</a>
-                                </p>
-                                <p>
-                                    <a href="#!">Lorem</a>
-                                </p>
-                            </div>
-
-                            <hr class="w-100 clearfix d-md-none" />
-
-                            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                                <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
-                                <p>
-                                    <a href="#!">Lorem</a>
-                                </p>
-                                <p>
-                                    <a href="#!">Lorem</a>
-                                </p>
-                                <p>
-                                    <a href="#!">Lorem</a>
-                                </p>
-                                <p>
-                                    <a href="#!">Lorem</a>
-                                </p>
-                            </div>
-
-                            <hr class="w-100 clearfix d-md-none" />
-
-                            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                                <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
-                                <p>
-                                    <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                                <p>
-                                    <i class="fas fa-envelope mr-3"></i> info @gmail.com</p>
-                                <p>
-                                    <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                                <p>
-                                    <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-                            </div>
-
-                        </div>
-
-                        <hr />
-
-                        <div class="row d-flex align-items-center">
-
-                            <div class="col-md-7 col-lg-8">
-
-                                <p class="text-center text-md-left">© 2020 Copyright:
-                                    <a href="https://www.facebook.com/people/Md-Imranul-Haque/100071106706650/">
-                                        <strong> MD Imranul Haque</strong>
-                                    </a>
-                                </p>
-
-                            </div>
-
-                            <div class="col-md-5 col-lg-4 ml-lg-0">
-
-                                <div class="text-center text-md-right">
-                                    <ul class="list-unstyled list-inline">
-                                        <li class="list-inline-item">
-                                        <FaFacebookSquare />
-                                        </li>
-                                        <li class="list-inline-item">
-                                        <FaTwitterSquare />
-                                        </li>
-                                        <li class="list-inline-item">
-                                        <FaGooglePlusSquare />
-                                        </li>
-                                        <li class="list-inline-item">
-                                        <FaLinkedin />
-                                        </li>
-                                    </ul>
+                <footer class="footer-section">
+                    <div class="container">
+                        <div class="footer-cta pt-5 pb-5">
+                            <div class="row">
+                                <div class="col-xl-4 col-md-4 mb-30">
+                                    <div class="single-cta">
+                                        <i class="fas fa-map-marker-alt"></i>
+                                        <div class="cta-text">
+                                            <h4>Find us</h4>
+                                            <span>FarmGate,Dhaka, Bangladesh</span>
+                                        </div>
+                                    </div>
                                 </div>
-
+                                <div class="col-xl-4 col-md-4 mb-30">
+                                    <div class="single-cta">
+                                        <i class="fas fa-phone"></i>
+                                        <div class="cta-text">
+                                            <h4>Call us</h4>
+                                            <span>+880 1245126</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xl-4 col-md-4 mb-30">
+                                    <div class="single-cta">
+                                        <i class="far fa-envelope-open"></i>
+                                        <div class="cta-text">
+                                            <h4>Mail us</h4>
+                                            <span>123@gmail.com</span>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-
                         </div>
-
+                        <div class="footer-content pt-5 pb-5">
+                            <div class="row">
+                                <div class="col-xl-4 col-lg-4 mb-50">
+                                    <div class="footer-widget">
+                                        <div class="footer-logo text-danger display-1">
+                                            Netflix
+                                        </div>
+                                        <div class="footer-text">
+                                            <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididuntut consec tetur adipisicing
+                                                elit,Lorem ipsum dolor sit amet.</p>
+                                        </div>
+                                        <div class="footer-social-icon">
+                                            <span>Follow us</span>
+                                            <a href="#"><i class="fab fa-facebook-f facebook-bg"></i></a>
+                                            <a href="#"><i class="fab fa-twitter twitter-bg"></i></a>
+                                            <a href="#"><i class="fab fa-google-plus-g google-bg"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+                                    <div class="footer-widget">
+                                        <div class="footer-widget-heading">
+                                            <h3>Useful Links</h3>
+                                        </div>
+                                        <ul>
+                                            <li><a href="#">Home</a></li>
+                                            <li><a href="#">about</a></li>
+                                            <li><a href="#">services</a></li>
+                                            <li><a href="#">portfolio</a></li>
+                                            <li><a href="#">Contact</a></li>
+                                            <li><a href="#">About us</a></li>
+                                            <li><a href="#">Our Services</a></li>
+                                            <li><a href="#">Expert Team</a></li>
+                                            <li><a href="#">Contact us</a></li>
+                                            <li><a href="#">Latest News</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
+                                    <div class="footer-widget">
+                                        <div class="footer-widget-heading">
+                                            <h3>Subscribe</h3>
+                                        </div>
+                                        <div class="footer-text mb-25">
+                                            <p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
+                                        </div>
+                                        <div class="subscribe-form">
+                                            <form action="#">
+                                                <input type="text" placeholder="Email Address"/>
+                                                    <button><i class="fab fa-telegram-plane"></i></button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-
+                    <div class="copyright-area">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xl-6 col-lg-6 text-center text-lg-left">
+                                    <div class="copyright-text">
+                                        <p>Copyright &copy; 2022, All Right Reserved <a href="https://www.linkedin.com/in/md-imranul-haque/">Md imranul Haque</a></p>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+                                    <div class="footer-menu">
+                                        <ul>
+                                            <li><a href="#">Home</a></li>
+                                            <li><a href="#">Terms</a></li>
+                                            <li><a href="#">Privacy</a></li>
+                                            <li><a href="#">Policy</a></li>
+                                            <li><a href="#">Contact</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </footer>
-            </div>
-        </div>
+      
 
     );
 };
